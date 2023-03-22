16c5651856a9f949ba395fa362e8efc48f12a674
Manish-621 <manishkmurali621@gmail.com>
2023-03-23 01:56:50 +0530

    initial(new features added)

---
+++ openedx/features/user_proctoring_system/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class UserProctoringSystemConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'openedx.features.user_proctoring_system'
